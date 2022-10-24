7c53a067d01edabb634b05f6160ed8cbf4ce0a6d
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 19:23:32 +1100

    Update project.py

--- project.py
+++ project.py
@@ -3,10 +3,12 @@ import streamlit as st
 import getpass
 database = {"aman.kharwal": "123456", "kharwal.aman": "654321"}
 username = st.text_input("Enter Your Username : ")
-password = st.text_input(getpass.getpass("Enter Your Password : "))
+password = st.text_input"Enter Your Password : ")
+password = getpass.getpass()
 for i in database.keys():
     if username == i:
         while password != database.get(i):
-            password = getpass.getpass("Enter Your Password Again : ")
+            password = st.text_input"Enter Your Password Again : ")
+            password = getpass.getpass()
         break
 print("Verified")
