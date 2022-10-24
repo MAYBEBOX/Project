70066054fc137745b45e4cbd4a92c2c4e4621681
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 21:57:33 +1100

    Update project.py

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ from PIL import ImageColor
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
+st.write(text)
 color = ImageColor.getcolor(text, "RGB")
 
 st.write(text, color = color)
