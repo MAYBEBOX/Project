136e798f73479ee093ef3c1a661055d3a33ee24a
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 20:49:09 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-coolor = st.color_picker('Choose Color Of Text', '#00f900')
+valiue = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
-st.write(text, color = coolor)
+value = value.lstrip('#')
+lv = len(value)
+return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+st.write(text, color = value)
