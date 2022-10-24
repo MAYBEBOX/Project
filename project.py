0a350d6d39a15c6ec135661f6c0d66c6485fc693
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 21:25:49 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-valiue = st.color_picker('Choose Color Of Text', '#00f900')
+value = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
 value = value.lstrip('#')
 lv = len(value)
