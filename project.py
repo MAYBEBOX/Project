72377a2c0092d6bd7d2c08cc353a26a5f033fa8c
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 21:50:19 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
-text = st.text_input("Enter Text: ")
-value = value.lstrip('#')
-lv = len(value)
-coloor = tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-st.write(text, color = coloor)
+text = st.text_input("Enter Text: ").lstrip('#')
+
+
+
+def hex_to_rgb(hex):
+  rgb = []
+  for i in (0, 2, 4):
+    decimal = int(hex[i:i+2], 16)
+    rgb.append(decimal)
+  
+  return tuple(rgb)
+st.write(text, color = hex_to_rgb(value))
