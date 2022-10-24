ef223c56a5cc9eeab48b92f2f4241e90227df33f
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 19:25:44 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
-import getpass
-database = {"aman.kharwal": "123456", "kharwal.aman": "654321"}
-username = st.text_input("Enter Your Username : ")
-password = st.text_input"Enter Your Password : ")
-password = getpass.getpass()
-for i in database.keys():
-    if username == i:
-        while password != database.get(i):
-            password = st.text_input"Enter Your Password Again : ")
-            password = getpass.getpass()
-        break
-print("Verified")
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
+
