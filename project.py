d43199273c314deb101731cb526d24fc340d609f
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 22:51:40 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-def hex_to_rgb(hex):
-  return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4)) 
+
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
- 
-st.write(text, color = hex_to_rgb)
+value.decode("hex")
+
+st.write(value)
