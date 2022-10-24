f7258afca73e884c81c24b981cfbea23b363d426
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 19:17:00 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-number = st.slider("Pick a number", 0, 100)
+import getpass
+database = {"aman.kharwal": "123456", "kharwal.aman": "654321"}
+username = st.text_input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
