67dde679bca6ee42f876071dc4784fbb8dfbe97b
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 20:37:15 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
 coolor = st.color_picker('Choose Color Of Text', '#00f900')
-if st.button('change', color = coolor):
-    st.write('Why hello there', color = coolor)
-else:
-    st.write('Goodbye', color = coolor)
+text = st.text_input("Enter Text: )
 st.write(text, color = coolor)
