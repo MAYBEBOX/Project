29b25ab4290355e24a45ff36bf104bfb4abe70e6
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 22:52:36 +1100

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,6 @@ import streamlit as st
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
-value.decode("hex")
-
+byte_array = bytearray.fromhex(value)
+byte_array.decode()
 st.write(value)
