a5b2bf49339689ca8d7b457b54f7f03cfd025fbd
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 21:56:50 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,15 +1,8 @@
 import streamlit as st
+from PIL import ImageColor
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
-text = st.text_input("Enter Text: ").lstrip('#')
+text = st.text_input("Enter Text: ")
+color = ImageColor.getcolor(text, "RGB")
 
-
-
-def hex_to_rgb(hex):
-  rgb = []
-  for i in (0, 2, 4):
-    decimal = int(hex[i:i+2], 16)
-    rgb.append(decimal)
-  
-  return tuple(rgb)
-st.write(text, color = hex_to_rgb(value))
+st.write(text, color = color)
