5b8f8b9179c19cd88e715a0be5fd01f1f3f3485e
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 20:37:28 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 coolor = st.color_picker('Choose Color Of Text', '#00f900')
-text = st.text_input("Enter Text: )
+text = st.text_input("Enter Text: ")
 st.write(text, color = coolor)
