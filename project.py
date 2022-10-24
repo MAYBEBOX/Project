1cba38414c410a91404f57b9659e9ecd672f9a89
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-24 20:12:16 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,5 @@
 import streamlit as st
 while True:
   color = st.color_picker('Choose Color Of Text', '#00f900')
-  
-  if color == '':
-    Print("Select Color")
-  else:
-    text = input("Input Mesaage")
-    st.write(text, color)
+  text = st.text_input("Input Mesaage")
+  st.write(text, color)
