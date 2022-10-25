64c3d5e005eeb94289be5e88a36d960f25b07275
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-25 11:15:42 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import sys
-import colorama
-from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
-
 
 RESET = '\033[0m'
 def get_color_escape(r, g, b, background=False):
@@ -11,5 +7,4 @@ def get_color_escape(r, g, b, background=False):
 
 value = st.color_picker('Choose Color Of Text', '#00f900')
 text = st.text_input("Enter Text: ")
-st.write(Fore.BLUE+Back.YELLOW+"Hi My name is Aman Kharwal "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
-st.write(get_color_escape(255, 128, 0) +text +get_color_escape(80, 30, 60, True))
+st.write(get_color_escape(255, 128, 0) +get_color_escape(80, 30, 60, True) +text + REST)
