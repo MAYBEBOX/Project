d802d82235feefc08d8ed308d31b210cbb4843cf
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-25 14:27:14 +1100

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-Height=st.text_input("Enter your height in centimeters: ")
-Weight=st.text_input("Enter your Weight in Kg: ")
+Height=st.text_input(float("Enter your height in centimeters: "))
+Weight=st.text_input(float("Enter your Weight in Kg: "))
 Height = float(Height)/100
-BMI= float(Weight)/(Height*Height)
+BMI= Weight/(Height*Height)
 st.write("your Body Mass Index is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
