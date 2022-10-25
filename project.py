a083a00fd55551cada21b3685dac6de9199c6989
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-25 11:25:19 +1100

    Update project.py

--- project.py
+++ project.py
@@ -7,13 +7,14 @@ BMI=Weight/(Height*Height)
 st.write("your Body Mass Index is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
-		print("you are severely underweight")
+		st.write("you are severely underweight")
 	elif(BMI<=18.5):
-		print("you are underweight")
+		st.write("you are underweight")
 	elif(BMI<=25):
-		print("you are Healthy")
+		st.write("you are Healthy")
 	elif(BMI<=30):
-		print("you are overweight")
-	else: print("you are severely overweight")
+		st.write("you are overweight")
+	else: 
+		st.write("you are severely overweight")
 else:
     st.write("enter valid details")
