9be3331e34d446ba3d1392bd2b069d661e5cdc32
MAYBEBOX <75935138+MAYBEBOX@users.noreply.github.com>
2022-10-25 14:12:11 +1100

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ import math
 
 Height=st.text_input("Enter your height in centimeters: ")
 Weight=st.text_input("Enter your Weight in Kg: ")
-Height = Height/100
+Height = Height/int(100)
 BMI=Weight/(Height*Height)
 st.write("your Body Mass Index is: ",BMI)
 if(BMI>0):
